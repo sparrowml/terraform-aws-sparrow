6e0317dfeac9f482c4e7be68ec05cf5294304ed8
jbencook <jbencook@protonmail.com>
2021-11-05 13:57:19 -0500

    return endpoint without port

--- modules/rds-instance/outputs.tf
+++ modules/rds-instance/outputs.tf
@@ -1,4 +1,4 @@
 output "host" {
-  value       = aws_db_instance.sparrow.endpoint
+  value       = split(":", aws_db_instance.sparrow.endpoint)[0]
   description = "The host of the database instance"
 }
