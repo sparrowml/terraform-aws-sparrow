b82a9fd12953a91c352b6d71beed6341a0112cde
jbencook <jbencook@protonmail.com>
2021-11-10 11:10:57 -0600

    improve ingress variables

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -1,36 +1,56 @@
+terraform {
+  experiments = [module_variable_optional_attrs]
+}
+
 data "http" "my_ip" {
   url = "http://ipv4.icanhazip.com"
+}
+
+locals {
+  ingress = [for i in var.ingress : defaults(i, {
+    description = ""
+    my_ip       = false
+    all_traffic = false
+    self        = false
+  })]
+  egress = [for e in var.egress : defaults(e, {
+    description = ""
+    my_ip       = false
+    all_traffic = false
+    self        = false
+  })]
 }
 
 resource "aws_security_group" "sparrow" {
   name   = var.name
   vpc_id = var.vpc_id
-  ingress = [for port in var.ingress_ports : {
-    from_port = port
-    to_port   = port
-    protocol  = "tcp"
 
-    cidr_blocks = concat(
-      var.my_ip ? ["${chomp(data.http.my_ip.body)}/32"] : [],
-      var.all_traffic ? ["0.0.0.0/0"] : []
-    )
-
-    description      = "Opening ${port}"
+  ingress = [for i in local.ingress : {
+    description      = i.description
+    from_port        = i.port
+    to_port          = i.port
+    protocol         = "tcp"
+    self             = i.self
+    security_groups  = i.security_group_id != null ? [i.security_group_id] : []
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
-    security_groups  = var.security_group_ids
-    self             = var.self
+
+    cidr_blocks = concat(
+      i.my_ip ? ["${chomp(data.http.my_ip.body)}/32"] : [],
+      i.all_traffic ? ["0.0.0.0/0"] : []
+    )
   }]
 
-  egress = var.egress ? [{
+  egress = var.all_egress ? [{
+    description      = "Opening outbound traffic"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = "Opening outbound traffic"
+    self             = false
+    security_groups  = []
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
-    security_groups  = []
-    self             = true
+
+    cidr_blocks = ["0.0.0.0/0"]
   }] : []
 }
--- modules/security-group/variables.tf
+++ modules/security-group/variables.tf
@@ -8,37 +8,34 @@ variable "vpc_id" {
   description = "ID of the VPC to put the security group in"
 }
 
-variable "ingress_ports" {
-  type        = list(number)
-  description = "Ports to open for TCP traffic"
-}
-
-variable "my_ip" {
-  type        = bool
-  description = "Whether to whitelist the developer's IP address"
-  default     = false
-}
-
-variable "all_traffic" {
-  type        = bool
-  description = "Whether to whitelist all traffic"
-  default     = false
-}
-
-variable "self" {
-  type        = bool
-  description = "Whether to whitelist other instances with the same security group"
-  default     = false
-}
-
-variable "security_group_ids" {
-  type        = list(string)
-  description = "A list of security group IDs to whitelist"
+variable "ingress" {
+  type = list(object({
+    port              = number
+    my_ip             = optional(bool)
+    all_traffic       = optional(bool)
+    security_group_id = optional(string)
+    self              = optional(bool)
+    description       = optional(string)
+  }))
+  description = "Ingress rules for the security group"
   default     = []
 }
 
 variable "egress" {
+  type = list(object({
+    port              = number
+    my_ip             = optional(bool)
+    all_traffic       = optional(bool)
+    security_group_id = optional(string)
+    self              = optional(bool)
+    description       = optional(string)
+  }))
+  description = "Egress rules for the security group"
+  default     = []
+}
+
+variable "all_egress" {
   type        = bool
   description = "Whether to allow outbound traffic to the internet"
-  default     = true
+  default     = false
 }
